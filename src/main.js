ca63a51eae203ae8df34c56d28c5fbbfdf2f8cd8
liuchao <hlions@163.com>
2021-01-21 13:00:01 +0800

    iview & axios interceptors configs

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import requests from "./apis/requests";
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
 
 Vue.config.productionTip = false;
+Vue.prototype.$requests = requests;
+Vue.use(ViewUI);
 
 new Vue({
   router,
